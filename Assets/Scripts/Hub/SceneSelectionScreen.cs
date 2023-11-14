6f06d419f02d4d47395b4f21b346f74a02f13c24
Maciej Sowiar <maciej.sowiar@gmail.com>
2023-11-14 21:15:34 +0100

    added screen controller

--- Assets/Scripts/Hub/SceneSelectionScreen.cs
+++ Assets/Scripts/Hub/SceneSelectionScreen.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SceneSelectionScreen : MonoBehaviour
+public class SceneSelectionScreen : ScreenBase
 {
+    public override ScreenIdentifiers Identifier() => ScreenIdentifiers.LevelSelect;
+
     [SerializeField] SceneLoader.SceneEnum scene = SceneLoader.SceneEnum.Map01;
     [SerializeField] Button loadMapButton;
 
---
+++ Assets/Scripts/Hub/ScreensController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum ScreenIdentifiers
+{
+    LevelSelect,
+    HeroSelect
+}
+
+public abstract class ScreenBase : MonoBehaviour
+{
+    public abstract ScreenIdentifiers Identifier();
+}
+
+public class ScreensController : Singleton<ScreensController>
+{
+    [SerializeField] List<ScreenBase> screens;
+    List<ScreenBase> spawnedScreens = new();
+    private ScreenBase currentScreen;
+    public void OpenScreen(ScreenIdentifiers id)
+    {
+        var screenToOpen = screens.FirstOrDefault(screen => screen.Identifier() == id);
+
+        if (screenToOpen == null)
+        {
+            Debug.LogError($"No screen with such ID {id}");
+            return;
+        }
+
+        if(currentScreen == null)
+        {
+            SpawnScreen(screenToOpen);
+            return;
+        }
+
+        if (currentScreen.Identifier() == id)
+        {
+            Debug.Log($"Screen {id} is currently opeend");
+            return;
+        }
+
+        var foundScreen = spawnedScreens.FirstOrDefault(x => x.Identifier() == id);
+
+        if(foundScreen)
+        {
+            foundScreen.gameObject.SetActive(true);
+            currentScreen.gameObject.SetActive(false);
+            currentScreen = foundScreen;
+            return;
+        }
+
+        currentScreen.gameObject.SetActive(false);
+        SpawnScreen(screenToOpen);
+    }
+
+    private void SpawnScreen(ScreenBase screenToSpawn)
+    {
+        var spawnedScreen = Instantiate(screenToSpawn);
+        spawnedScreens.Add(spawnedScreen);
+        currentScreen = spawnedScreen;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            OpenScreen(ScreenIdentifiers.LevelSelect);
+        if (Input.GetKeyDown(KeyCode.B))
+            OpenScreen(ScreenIdentifiers.HeroSelect);
+    }
+}
